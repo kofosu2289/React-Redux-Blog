a2abf3865a27ebcef0cf7ac23c7e5c85afe3e49f
Kenneth Ofosu <kennethofosu01@gmail.com>
2019-03-21 12:58:59 -0400

    delete fetchMessage action and its corresponding reducer

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -1,20 +1,20 @@
 import {
   AUTH_USER,
   UNAUTH_USER,
-  FETCH_MESSAGE,
-  CHECK_AUTHORITY
-} from "../actions/types";
+
+  CHECK_AUTHORITY,
+} from '../actions/types';
 
 export default function(state = {}, action) {
-  switch (action.type) {
+  switch(action.type) {
     case AUTH_USER:
       return { ...state, authenticated: true, username: action.payload };
     case UNAUTH_USER:
-      return { ...state, authenticated: false, username: "" };
-    case FETCH_MESSAGE:
-      return { ...state, message: action.payload };
-    case CHECK_AUTHORITY:
+      return { ...state, authenticated: false, username: '' };
+
+    case CHECK_AUTHORITY:  // check if the user has the authority to make change to a specific post
       return { ...state, allowChange: action.payload };
+
     default:
       return state;
   }
